7a8037ec45d8cea272c68f9d45185983fb692af7
nhithanh1998 <69423339+nhithanh1998@users.noreply.github.com>
2021-05-28 15:13:25 +0700

    update

--- src/modules/apis/models/models.module.ts
+++ src/modules/apis/models/models.module.ts
@@ -3,9 +3,10 @@ import { ModelsService } from './models.service';
 import { ModelsController } from './models.controller';
 import { Model } from './entities/model.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Style } from '../styles/entities/style.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Model])],
+  imports: [TypeOrmModule.forFeature([Model, Style])],
   controllers: [ModelsController],
   providers: [ModelsService],
   exports: [ModelsService]
