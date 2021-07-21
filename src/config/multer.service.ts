e3ddc074a1505df34da541f4c0cc90df9549b449
nhithanh1998 <30461021+nhatthanh1998@users.noreply.github.com>
2021-07-21 13:30:21 +0700

    update

--- src/config/multer.service.ts
+++ src/config/multer.service.ts
@@ -75,3 +75,27 @@ export const uploadSnapshotOption = (s3: S3) => {
         })
     }
 };
+
+export const uploadTrainingsOption = (s3: S3) => {
+    return {
+        fileFilter: (req: any, file: any, cb: any) => {
+            if (file.mimetype.match(/\/(jpg|jpeg|png)$/)) {
+                cb(null, true);
+            } else {
+                cb(new HttpException({
+                    status: 400,
+                    message: `Unsupported file type ${extname(file.originalname)}`
+                }, HttpStatus.BAD_REQUEST), false);
+            }
+        },
+        storage: multerS3({
+            s3: s3,
+            bucket: 'artisan-photos',
+            key: function (req: any, file, cb) {
+                let destination = `trainings/${new Date().toISOString().substring(0, 10)}/${Date.now().toString()}`
+                cb(null, destination)
+            },
+            contentType: multerS3.AUTO_CONTENT_TYPE
+        })
+    }
+};
